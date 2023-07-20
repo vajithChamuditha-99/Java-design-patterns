46d3d7ebadf9dd9f8208c7e17c8081a168a1a6d7
Vajith Chamuditha <77768444+vajithChamuditha-99@users.noreply.github.com>
2023-07-20 21:44:54 +0530

    Add Concrete Factory 2

---
+++ src/AbstractFactoryPattern/ConcreteFactory1.java
@@ -0,0 +1,13 @@
+package AbstractFactoryPattern;
+
+class ConcreteFactory1 implements AbstractFactory {
+    @Override
+    public ProductA createProductA() {
+        return new ConcreteProductA1();
+    }
+
+    @Override
+    public ProductB createProductB() {
+        return new ConcreteProductB1();
+    }
+}
---
+++ src/AbstractFactoryPattern/ConcreteFactory2.java
@@ -0,0 +1,13 @@
+package AbstractFactoryPattern;
+
+class ConcreteFactory2 implements AbstractFactory {
+    @Override
+    public ProductA createProductA() {
+        return new ConcreteProductA2();
+    }
+
+    @Override
+    public ProductB createProductB() {
+        return new ConcreteProductB2();
+    }
+}
