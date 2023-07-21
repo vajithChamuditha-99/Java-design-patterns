c4d587db5ff1cd305fce01f277a04339064ddb0c
Vajith Chamuditha <77768444+vajithChamuditha-99@users.noreply.github.com>
2023-07-21 19:38:18 +0530

    Singleton pattern complete

--- src/SingletonPattern/Main.java
+++ src/SingletonPattern/Main.java
@@ -2,6 +2,11 @@ package SingletonPattern;
 
 public class Main {
     public static void main(String[] args) {
+        // Get the singleton instance
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
 
+        // Both references point to the same instance
+        System.out.println(singleton1 == singleton2); // Output: true
     }
 }
